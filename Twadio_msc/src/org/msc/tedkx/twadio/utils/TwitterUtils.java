01f284ef0b130f78aae9f26fb62db041b2071bb2
tedkx <thodwriskalaitzidis@hotmail.com>
2012-12-04 17:17:57 +0200

    wrote down keys

---
+++ Twadio_msc/src/org/msc/tedkx/twadio/utils/TwitterUtils.java
@@ -0,0 +1,9 @@
+package org.msc.tedkx.twadio.utils;
+
+public abstract class TwitterUtils {
+	//http://blog.notdot.net/2010/02/Writing-a-twitter-service-on-App-Engine
+	public static final String consumer_key = "5ZTWoyxkxryLkq5sMWt5w";
+	public static final String consumer_secret = "VLU2Kt5Cxf5ccfrvsYkrsSba41EKYLs55GDnt3XEo5s";
+	public static final String callback_url = "http://www.myurl.com/callback/twitter";
+
+}
