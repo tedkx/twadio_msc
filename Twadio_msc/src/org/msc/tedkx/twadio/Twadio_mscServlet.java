80a0cbd4d292c2ff3cf97a4a4a5a2649e29a4182
tedkx <thodwriskalaitzidis@hotmail.com>
2012-12-04 14:40:56 +0200

    first commit

---
+++ Twadio_msc/src/org/msc/tedkx/twadio/Twadio_mscServlet.java
@@ -0,0 +1,11 @@
+package org.msc.tedkx.twadio;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Twadio_mscServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
